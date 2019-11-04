3896b4e2194c06fe6674d41a304571eccb3b2688
tabayashi <shinichiro.tabayashi@aisin.co.jp>
2019-11-04 15:01:19 +0900

    add sample code

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Score-CAM
+
+![](./image/sample_output.png)
+
+An implementation of [Score-CAM](https://arxiv.org/abs/1910.01279) with keras
+
+The following methods are compared.
+
+- Grad-CAM
+- Grad-CAM++
+- Guided Backpropagation
+- Faster-Score-CAM (Original faster version of Score-CAM)
+
+In addition, the followings are contained.
+
+- Visualization of anomalous area for [**DAGM dataset**](https://resources.mpi-inf.mpg.de/conference/dagm/2007/prizes.html)
+- Sample code for applying Score-CAM to your model.
+
+![](./result/Class6_result_0.png)
+
+# Requirement
+
+- Python 3.6.8
+- Keras 2.2.4
+- tensorflow 1.14.0
+
+# Usage
+
+See `Score-CAM.ipynb`.
---
+++ gradcamutils.py
@@ -0,0 +1,170 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+from keras.backend import tensorflow_backend
+from keras import backend as K
+from keras.models import Model
+
+def normalize(x):
+        """Utility function to normalize a tensor by its L2 norm"""
+        return (x + 1e-10) / (K.sqrt(K.mean(K.square(x))) + 1e-10)
+
+def GradCam(model, img_array, layer_name):
+    cls = np.argmax(model.predict(img_array))
+    
+    """GradCAM method for visualizing input saliency."""
+    y_c = model.output[0, cls]
+    conv_output = model.get_layer(layer_name).output
+    grads = K.gradients(y_c, conv_output)[0]
+    # grads = normalize(grads)
+
+    gradient_function = K.function([model.input], [conv_output, grads])
+    output, grads_val = gradient_function([img_array])
+    output, grads_val = output[0, :], grads_val[0, :, :, :]
+    weights = np.mean(grads_val, axis=(0, 1))
+
+    cam = np.dot(output, weights)
+    cam = np.maximum(cam, 0)  # Passing through ReLU
+    cam /= np.max(cam)  # scale 0 to 1.0  
+
+    return cam
+
+def GradCamPlusPlus(model, img_array, layer_name):
+    cls = np.argmax(model.predict(img_array))
+    y_c = model.output[0, cls]
+    conv_output = model.get_layer(layer_name).output
+    grads = K.gradients(y_c, conv_output)[0]
+    # grads = normalize(grads)
+
+    first = K.exp(y_c)*grads
+    second = K.exp(y_c)*grads*grads
+    third = K.exp(y_c)*grads*grads*grads
+
+    gradient_function = K.function([model.input], [y_c,first,second,third, conv_output, grads])
+    y_c, conv_first_grad, conv_second_grad,conv_third_grad, conv_output, grads_val = gradient_function([img_array])
+    global_sum = np.sum(conv_output[0].reshape((-1,conv_first_grad[0].shape[2])), axis=0)
+
+    alpha_num = conv_second_grad[0]
+    alpha_denom = conv_second_grad[0]*2.0 + conv_third_grad[0]*global_sum.reshape((1,1,conv_first_grad[0].shape[2]))
+    alpha_denom = np.where(alpha_denom != 0.0, alpha_denom, np.ones(alpha_denom.shape))
+    alphas = alpha_num/alpha_denom
+
+    weights = np.maximum(conv_first_grad[0], 0.0)
+    alpha_normalization_constant = np.sum(np.sum(alphas, axis=0),axis=0)
+    alphas /= alpha_normalization_constant.reshape((1,1,conv_first_grad[0].shape[2]))
+    deep_linearization_weights = np.sum((weights*alphas).reshape((-1,conv_first_grad[0].shape[2])),axis=0)
+
+    cam = np.sum(deep_linearization_weights*conv_output[0], axis=2)
+    cam = np.maximum(cam, 0)  # Passing through ReLU
+    cam /= np.max(cam) # scale 0 to 1.0  
+
+    return cam
+
+def softmax(x):
+    f = np.exp(x)/np.sum(np.exp(x), axis = 1, keepdims = True)
+    return f
+
+def ScoreCam(model, img_array, layer_name, max_N=-1):
+
+    cls = np.argmax(model.predict(img_array))
+    act_map_array = Model(inputs=model.input, outputs=model.get_layer(layer_name).output).predict(img_array)
+    
+    # extract effective maps
+    if max_N != -1:
+        act_map_std_list = [np.std(act_map_array[0,:,:,k]) for k in range(act_map_array.shape[3])]
+        unsorted_max_indices = np.argpartition(-np.array(act_map_std_list), max_N)[:max_N]
+        max_N_indices = unsorted_max_indices[np.argsort(-np.array(act_map_std_list)[unsorted_max_indices])]
+        act_map_array = act_map_array[:,:,:,max_N_indices]
+
+    input_shape = model.layers[0].output_shape[1:]  # get input shape
+    # 1. upsampled to original input size
+    act_map_resized_list = [cv2.resize(act_map_array[0,:,:,k], input_shape[:2], interpolation=cv2.INTER_LINEAR) for k in range(act_map_array.shape[3])]
+    # 2. normalize the raw activation value in each activation map into [0, 1]
+    act_map_normalized_list = []
+    for act_map_resized in act_map_resized_list:
+        if np.max(act_map_resized) - np.min(act_map_resized) != 0:
+            act_map_normalized = act_map_resized / (np.max(act_map_resized) - np.min(act_map_resized))
+        else:
+            act_map_normalized = act_map_resized
+        act_map_normalized_list.append(act_map_normalized)
+    # 3. project highlighted area in the activation map to original input space by multiplying the normalized activation map
+    masked_input_list = []
+    for act_map_normalized in act_map_normalized_list:
+        masked_input = np.copy(img_array)
+        for k in range(3):
+            masked_input[0,:,:,k] *= act_map_normalized
+        masked_input_list.append(masked_input)
+    masked_input_array = np.concatenate(masked_input_list, axis=0)
+    # 4. feed masked inputs into CNN model and softmax
+    pred_from_masked_input_array = softmax(model.predict(masked_input_array))
+    # 5. define weight as the score of target class
+    weights = pred_from_masked_input_array[:,cls]
+    # 6. get final class discriminative localization map as linear weighted combination of all activation maps
+    cam = np.dot(act_map_array[0,:,:,:], weights)
+    cam = np.maximum(0, cam)  # Passing through ReLU
+    cam /= np.max(cam)  # scale 0 to 1.0
+    
+    return cam
+
+def superimpose(original_img_path, cam, emphasize=False):
+    
+    img_bgr = cv2.imread(original_img_path)
+
+    heatmap = cv2.resize(cam, (img_bgr.shape[1], img_bgr.shape[0]))
+    if emphasize:
+        heatmap = sigmoid(heatmap, 50, 0.5, 1)
+    heatmap = np.uint8(255 * heatmap)
+    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+    
+    hif = .8
+    superimposed_img = heatmap * hif + img_bgr
+    superimposed_img = np.minimum(superimposed_img, 255.0).astype(np.uint8)  # scale 0 to 255  
+    superimposed_img_rgb = cv2.cvtColor(superimposed_img, cv2.COLOR_BGR2RGB)
+    
+    return superimposed_img_rgb
+
+import keras
+import keras.backend as K
+
+import tensorflow as tf
+from tensorflow.python.framework import ops
+from keras.applications.vgg16 import preprocess_input
+
+def build_guided_model(build_model_function):
+    """Function returning modified model.
+    
+    Changes gradient function for all ReLu activations according to Guided Backpropagation.
+    """
+    if "GuidedBackProp" not in ops._gradient_registry._registry:
+        @ops.RegisterGradient("GuidedBackProp")
+        def _GuidedBackProp(op, grad):
+            dtype = op.inputs[0].dtype
+            return grad * tf.cast(grad > 0., dtype) * \
+                   tf.cast(op.inputs[0] > 0., dtype)
+
+    g = tf.get_default_graph()
+    with g.gradient_override_map({'Relu': 'GuidedBackProp'}):
+        new_model = build_model_function()
+    return new_model
+
+def GuidedBackPropagation(model, img_array, layer_name):
+    model_input = model.input
+    layer_output = model.get_layer(layer_name).output
+    max_output = K.max(layer_output, axis=3)
+    get_output = K.function([model_input], [K.gradients(max_output, model_input)[0]])
+    saliency = get_output([img_array])
+    saliency = np.clip(saliency[0][0], 0.0, 1.0)  # scale 0 to 1.0  
+    return saliency
+
+def sigmoid(x, a, b, c):
+    return c / (1 + np.exp(-a * (x-b)))
+
+from keras.preprocessing.image import load_img, img_to_array
+
+def read_and_preprocess_img(path, size=(224,224)):
+    img = load_img(path, target_size=size)
+    x = img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
